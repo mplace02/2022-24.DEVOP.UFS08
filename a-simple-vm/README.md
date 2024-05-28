a8ca37356eb67c7854719129e302e9da891f56e0
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-05-28 21:18:37 +0000

    fix: tf demo

---
+++ a-simple-vm/README.md
@@ -0,0 +1,5 @@
+# Terraform howtoss
+
+## Use environment variables to define Terraform input variables
+
+`TF_VAR_GOOGLE_CLOUD_PROJECT_ID=exalted-pattern-424313-j3 terraform apply`
---
+++ a-simple-vm/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+variable "GOOGLE_CLOUD_PROJECT_ID" {
+  type = string
+}
+
+provider "google" {
+  project = "${var.GOOGLE_CLOUD_PROJECT_ID}"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "jon:${file(pathexpand("~/.ssh/id_rsa.pub"))}"
+  }
+}
